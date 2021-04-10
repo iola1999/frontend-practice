673cdeea025644d533a1501bd34b20cd28df0ba1
iola1999 <iola1999@foxmail.com>
2021-04-11 00:32:57 +0800

    再测试一下LRUCache，水个两数之和

--- leetcode/leetCode146.LRU缓存机制.ts
+++ leetcode/leetCode146.LRU缓存机制.ts
@@ -1,8 +1,18 @@
-// interface toCacheUnity {
-//     [propName: any]: any;
-// }
+/**
+ * 
+146. LRU 缓存机制
+运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+实现 LRUCache 类：
 
-class LRUCache {
+LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ 
+
+进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
+ */
+
+export class LRUCache {
   maxSize: number;
   cacheMap: object;
   keys: (number | string)[];
@@ -14,19 +24,19 @@ class LRUCache {
   }
 
   put(key: number | string, value: any) {
-    console.log("存入", key, value);
+    // console.log("存入", key, value);
     // 存入。调用 updateKeys
     this.cacheMap[key] = value;
     this.updateKeys(key);
-    console.log("当前缓存", JSON.stringify(this.cacheMap), "当前lru队列", this.keys);
+    // console.log("当前缓存", JSON.stringify(this.cacheMap), "当前lru队列", this.keys);
   }
 
   get(key: number | string): any {
-    console.log("读取", key);
+    // console.log("读取", key);
     // 如果找到了缓存，updateKeys 这个key到最后。如果没有找到，返回undefined
     const value = this.cacheMap[key];
     value !== undefined && this.updateKeys(key); // 没有找到缓存的话，不需要调用
-    console.log("当前缓存", JSON.stringify(this.cacheMap), "当前lru队列", this.keys);
+    // console.log("当前缓存", JSON.stringify(this.cacheMap), "当前lru队列", this.keys);
     return value === undefined ? -1 : value;
   }
 
@@ -47,18 +57,11 @@ class LRUCache {
   }
 
   pruneFirstCache() {
-    console.log("作废最久未使用的缓存：", this.keys[0], this.cacheMap[this.keys[0]]);
+    // console.log("作废最久未使用的缓存：", this.keys[0], this.cacheMap[this.keys[0]]);
     delete this.cacheMap[this.keys[0]];
     this.keys.shift();
   }
 }
-
-// const lrucache: LRUCache = new LRUCache(3);
-// lrucache.put(1, 'one');
-// lrucache.put(2, 'two');
-// console.log('get 1:', lrucache.get(1));
-// lrucache.put(3, 'three');
-// lrucache.put(4, 'four');
 
 /**
  * 输入：
