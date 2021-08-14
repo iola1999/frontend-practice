e0cafec09aef19a9778044986d2ffaf9a1b07f74
iola1999 <iola1999@foxmail.com>
2021-08-14 15:39:58 +0800

    卷起来了

--- basics-practice/works-by-day/0-CodeRunner.js
+++ basics-practice/works-by-day/0-CodeRunner.js
@@ -1,35 +1,9 @@
-function delay(ms) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, ms);
-    });
-}
+const aaa = [1, 2, 3, 4, 5]
 
-function* gen() {
-    yield console.log(1)
-    yield delay(1000).then(() => {
-        console.log(2)
-    })
-    yield console.log(3)
-}
-
-// 先测试一下基础原理
-const step = gen();
-// step.next()
-// step.next().value.then(() => {
-//     step.next()
-// })
-
-function change(genedFunc) {
-    const item = genedFunc.next();
-    const {value, done} = item
-    if (done) return value;
-    if (value instanceof Promise) {
-        value.then(() => {
-            change(genedFunc)
-        })
-    } else {
-        change(genedFunc)
+for (let i = 0; i < aaa.length; i++) {
+    console.log(i, aaa[i])
+    if (aaa[i] === 2) {
+        aaa.splice(i, 1)
+        i--
     }
 }
-
-change(step)
